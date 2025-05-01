4d845fdb6e4800e0ce3330557995a3a70db83edd
danielfigueroa1879 <90109125+danielfigueroa1879@users.noreply.github.com>
2025-05-01 04:32:37 -0400

    Update pdf-optimizado.js

--- pdf-optimizado.js
+++ pdf-optimizado.js
@@ -99,7 +99,7 @@ function generarPDFCorregido() {
             });
             
             titulo.style.position = 'relative';
-            titulo.style.marginLeft = '20px';  // Reducido para mover a la izquierda
+            titulo.style.marginLeft = '10px';  // Reducido para mover a la izquierda
             titulo.style.marginTop = '15px';
             titulo.style.fontSize = '19pt';
         }
